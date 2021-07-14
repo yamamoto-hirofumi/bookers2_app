e8dad49779e37fe3a7ff8e0f83911313ebdccbde
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-07-14 14:45:08 +0000

    [update]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books, only: [:new, :create, :index, :show, :destroy]
 end
