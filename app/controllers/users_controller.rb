fc49d9159cbef668643d20799c0c1d359026bfd1
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-07-19 06:29:48 +0000

    [finish]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
   def edit
     @user =User.find(params[:id])
-    if @user == current_user
-      render "edit"
-    else
-      redirect_to books_path
+    if @user != current_user
+      redirect_to user_path(current_user)
     end
   end
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
    validates :name, presence: true, length: { in: 2..20 }, uniqueness: { case_sensitive: false }
-   validates :introduction, presence: true, length: { maximum: 50 }
+   validates :introduction, length: { maximum: 50 }
 end
